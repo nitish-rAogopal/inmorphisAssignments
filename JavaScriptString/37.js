2606f311c37ecf13d288895e34824e93dc284fe7
nitish-rAogopal <nitish.raogopal@gmail.com>
2024-07-15 12:13:52 +0530

    Assignment

--- JavaScriptString/37.js
+++ JavaScriptString/37.js
@@ -5,3 +5,13 @@
 //     true
 //     console.log(compare_strings('ABCD', 'Abce'));
 //     false
+
+function compare_strings(str1, str2) {
+    var pat_str1 = new RegExp(str1, 'gi');
+    var pat_str2 = new RegExp(str2, 'gi');
+    if (pat_str1.test(str2) && pat_str2.test(str1)) return true;
+    else return false;
+}
+
+console.log(compare_strings('abcd', 'AbcD')); // true
+console.log(compare_strings('ABCD', 'Abce')); // false
